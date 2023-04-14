069b67fe7bc2cee9380225ca87c0c699e06acea8
huangjinxing <731727479@qq.com>
2023-04-14 17:34:00 +0800

    dispatch

---
+++ todoList_dispatcher/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <todo-list />
+</template>
+
+<script>
+import TodoList from './components/TodoList/index.vue'
+export default {
+  components: { TodoList }
+}
+
+</script>
+
+<style scoped>
+
+</style>
